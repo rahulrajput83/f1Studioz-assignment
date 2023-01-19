013b8ad194b1fd9841871d3ba3558e8ccf049464
Rahul Rajput <rahulrajput21112@gmail.com>
2023-01-19 15:36:51 +0530

    Link in Page 2

--- src/Pages/Form1.jsx
+++ src/Pages/Form1.jsx
@@ -40,13 +40,13 @@ function Form1() {
       <div className='d-flex flex-column p-3 gap-3 justify-content-center mt-0'>
         {Data.map((e, i) => {
           return (
-            <div key={`form1-${i}`} className='px-3 py-2 d-flex flex-row align-items-center justify-content-between gap-2 col mx-lg-auto col-lg-6 shadow p-3 bg-white rounded-1'>
+            <Link to='/form2' key={`form1-${i}`} className='text-decoration-none px-3 py-2 d-flex flex-row align-items-center justify-content-between gap-2 col mx-lg-auto col-lg-6 shadow p-3 bg-white rounded-1'>
               <div className='d-flex w-100 flex-column'>
                 <span className='text-textDarkGray weight fs-6'>{e.title}</span>
                 <span className='text-darkGray size'>{e.desc}</span>
               </div>
               <FaAngleRight className='text-brown' />
-            </div>
+            </Link>
           )
         })}
       </div>
